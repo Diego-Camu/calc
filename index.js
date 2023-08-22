fbbfdff6c228c770b85d8a01f79d0a625f7ddd7c
Diego-Camu <diegopa7@gmail.com>
2023-08-22 17:44:39 -0300

    a mesma bosta de antes

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 function soma(){
     var num1 = document.getElementById("inputnum1").value;
     var num2 = document.getElementById("inputnum2").value;
-   alert(Number(num1)+Number(num2));
+    var result=(Number(num1)+Number(num2));
+    var res = window.document.querySelector("#abc");
+    res.innerHTML=result;s
 }
 function sub(){
     var num1 = document.getElementById("inputnum1").value;
     var num2 = document.getElementById("inputnum2").value;
-    alert(Number(num1)-Number(num2));
+    var result=(Number(num1)-Number(num2));
 }
 function div(){
     var num1 = document.getElementById("inputnum1").value;
     var num2 = document.getElementById("inputnum2").value;
-    alert(Number(num1)/Number(num2));
+    var result=(Number(num1)/Number(num2));
 }
 function mult(){
     var num1 = document.getElementById("inputnum1").value;
     var num2 = document.getElementById("inputnum2").value;
-    alert(Number(num1)*Number(num2));
+    var result=(Number(num1)*Number(num2));
 }
-vvv
+
