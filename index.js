8379a1be3271b9c7cb3b1c75d6a370c2dfd8f70a
Diego-Camu <diegopa7@gmail.com>
2023-08-22 17:23:51 -0300

    teste

---
+++ index.js
@@ -0,0 +1,20 @@
+function soma(){
+    var num1 = document.getElementById("inputnum1").value;
+    var num2 = document.getElementById("inputnum2").value;
+   alert(Number(num1)+Number(num2));
+}
+function sub(){
+    var num1 = document.getElementById("inputnum1").value;
+    var num2 = document.getElementById("inputnum2").value;
+    alert(Number(num1)-Number(num2));
+}
+function div(){
+    var num1 = document.getElementById("inputnum1").value;
+    var num2 = document.getElementById("inputnum2").value;
+    alert(Number(num1)/Number(num2));
+}
+function mult(){
+    var num1 = document.getElementById("inputnum1").value;
+    var num2 = document.getElementById("inputnum2").value;
+    alert(Number(num1)*Number(num2));
+}
