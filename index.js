7554d0333b5aee69e167df1b686807b44572a13d
Diego-Camu <diegopa7@gmail.com>
2023-08-30 11:09:21 -0300

    oi fodase

--- index.js
+++ index.js
@@ -3,21 +3,28 @@ function soma(){
     var num2 = document.getElementById("inputnum2").value;
     var result=(Number(num1)+Number(num2));
     var res = window.document.querySelector("#abc");
-    res.innerHTML=result;s
+    res.innerHTML=result;
 }
 function sub(){
     var num1 = document.getElementById("inputnum1").value;
     var num2 = document.getElementById("inputnum2").value;
     var result=(Number(num1)-Number(num2));
+    var res = window.document.querySelector("#abc");
+    res.innerHTML=result;
 }
 function div(){
     var num1 = document.getElementById("inputnum1").value;
     var num2 = document.getElementById("inputnum2").value;
-    var result=(Number(num1)/Number(num2));
+    var result=(Number(num1)/Number(num2)).toFixed(2);
+    var res = window.document.querySelector("#abc");
+    res.innerHTML=result ;
 }
 function mult(){
     var num1 = document.getElementById("inputnum1").value;
     var num2 = document.getElementById("inputnum2").value;
     var result=(Number(num1)*Number(num2));
+    var res = window.document.querySelector("#abc");
+    res.innerHTML=result;
 }
 
+     
